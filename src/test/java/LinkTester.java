1d61f9ab657ecd7104259a665c65c86ab5a5ef0e
DavidSinz <davidsinz@gmx.net>
2022-05-10 12:52:02 +0200

    iterate through links

--- src/test/java/LinkTester.java
+++ src/test/java/LinkTester.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.Iterator;
+import java.util.List;
 
 public class LinkTester {
 	
@@ -18,24 +20,26 @@ public class LinkTester {
 	public void setupTest() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 	}
 
 	@Test
 	public void test() {
-        driver.get("https://google.com");
+        driver.get("http://einlegesohlentest.de");
         
-        System.out.print(driver.getTitle()); // => "Google"
+        System.out.print(driver.getTitle());
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         
-        WebElement searchBox = driver.findElement(By.name("q"));
-        WebElement searchButton = driver.findElement(By.name("btnK"));
+        String url = "";
+        List<WebElement> allURLs = driver.findElements(By.tagName("a"));
+        System.out.println("Total links on the webpage: " + allURLs.size());
         
-        searchBox.sendKeys("Selenium");
-        searchButton.click();
-        
-        searchBox = driver.findElement(By.name("q"));
-        searchBox.getAttribute("value"); // => "Selenium"
+        Iterator<WebElement> iterator = allURLs.iterator();
+        while (iterator.hasNext()) {
+        	url = iterator.next().getText();
+        	System.out.println(url);
+        }
 	}
 	
 	@After
