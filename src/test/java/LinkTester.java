72ea4c900dcb71c6767fe77ae8128b7888089f72
DavidSinz <davidsinz@gmx.net>
2022-05-10 13:38:08 +0200

    checking links if they are broken

--- src/test/java/LinkTester.java
+++ src/test/java/LinkTester.java
@@ -4,12 +4,15 @@ import java.time.Duration;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.Iterator;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class LinkTester {
@@ -25,21 +28,32 @@ public class LinkTester {
 
 	@Test
 	public void test() {
-        driver.get("http://einlegesohlentest.de");
-        
-        System.out.print(driver.getTitle());
+        driver.get("http://einlegesohlentest.de/einlegesohlen/");
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         
-        String url = "";
-        List<WebElement> allURLs = driver.findElements(By.tagName("a"));
-        System.out.println("Total links on the webpage: " + allURLs.size());
+        List<WebElement> aLinks = driver.findElements(By.tagName("a"));
+        System.out.println("Total links on the webpage: " + aLinks.size());
         
-        Iterator<WebElement> iterator = allURLs.iterator();
-        while (iterator.hasNext()) {
-        	url = iterator.next().getText();
-        	System.out.println(url);
+        for (int i = 0; i < aLinks.size(); i++) {
+        	WebElement el = aLinks.get(i);
+        	String url = el.getAttribute("href");
+        	verifyLinks(url);
         }
+	}
+	
+	public void verifyLinks(String linkURL) {
+		try {
+			URL url = new URL(linkURL);
+			
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setConnectTimeout(5000);
+			con.connect();
+			
+			if (con.getResponseCode() >= 400)
+				System.out.println(linkURL + " - " + con.getResponseMessage() + " is a broken link");
+			else System.out.println(linkURL + " - " + con.getResponseMessage());
+		} catch (Exception e) {}
 	}
 	
 	@After
